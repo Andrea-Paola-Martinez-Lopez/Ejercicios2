0457469190524e1cf22af63d0d174fe8600f0c16
Andrea <andrea.paola.martinez.lopez12@gmail.com>
2022-10-01 20:32:38 -0500

    Initial commit

--- src/main/kotlin/Ejercicios/Ejercicio2.kt
+++ src/main/kotlin/Ejercicios/Ejercicio2.kt
@@ -1,4 +1,22 @@
 package Ejercicios
 
 class Ejercicio2 {
+<<<<<<< HEAD
 }
+=======
+}
+fun sumarLista(num: Array<Int> , operacion:(Int)->Int): Array<Int> {
+    for (posicion in 0..num.size-1){
+        num.set(posicion,operacion(num.get(posicion)))
+        println(num.get(posicion))
+    }
+    return num
+}
+fun sumar(num: Int): Int{
+    return num+10
+}
+ fun main() {
+     val numeros = arrayOf<Int>(1,2,3,4,5,6,7,8,9);
+     sumarLista(numeros, ::sumar)
+ }
+>>>>>>> 5c749eb (Initial commit)
